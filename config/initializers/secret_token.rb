a63f80217886bac13d52398f51c97d70785b85be
bpgabrielli <bpgabrielli@gmail.com>
2015-03-09 18:16:07 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Honeycomb::Application.config.secret_key_base = 'a514944cef9ad171e3a81b5e22dbacdaa3e2652b6536f559a200a619acb3df6f44c006ce2ea0ffcd2cff856553dee47c62f56cdd4eec0f519d0b2eb28be3003b'
